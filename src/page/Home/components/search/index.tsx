0d247aba7b58f4552f115c7abf98a7a070dcaaee
ObidjonFrontEndn <lightline002@gmail.com>
2025-05-30 22:50:17 +0500

    3.1.4

--- src/page/Home/components/search/index.tsx
+++ src/page/Home/components/search/index.tsx
@@ -9,7 +9,7 @@ import { useTranslation } from 'react-i18next'
 const Search = () => {
 	const [search , setSearch ] = useState<string>("")
 		const { t } = useTranslation()
-		const {_isOpenSearchModal, setOpenSearchMenu } = useSearchModal()
+		const { setOpenSearchMenu } = useSearchModal()
 
 	return (
 		<div className='flex w-full px-[15px] md:px-[0] md:w-[60%] flex-col md:flex-row mx-auto gap-[10px]'>
